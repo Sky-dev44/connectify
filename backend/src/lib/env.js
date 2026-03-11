6defff9ef94903bdb7d7c97f2cb4cd2a3be1699e
Sky-dev44 <jagotshantichakma@gmail.com>
2026-03-11 15:43:40 +0600

    added profile pic uploader functionality

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -9,4 +9,7 @@ export const ENV = {
   EMAIL_FROM: process.env.EMAIL_FROM,
   EMAIL_FROM_Name: process.env.EMAIL_FROM_Name,
   CLIENT_URL: process.env.CLIENT_URL,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const app = express();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); //req.body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
