618f32171382b9e492bd4a21d28d10ab33847116
Sky-dev44 <jagotshantichakma@gmail.com>
2026-03-21 12:29:46 +0600

    setup socket at backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,9 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-
-const app = express();
 
 const PORT = ENV.PORT || 3000;
 
@@ -25,7 +24,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
